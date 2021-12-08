2b3b5da4eae2c1911168f2a26db5b4d4c1b3fbd6
Helder Medeiros <hmedeiros211@gmail.com>
2021-12-08 15:02:23 -0300

    em execucao os exercicios calendario trybe

--- 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/calendario-trybe/script.js
+++ 01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/calendario-trybe/script.js
@@ -12,5 +12,39 @@ function createDaysOfTheWeek() {
 };
 
 createDaysOfTheWeek();
+//
 
-// Escreva seu código abaixo.
+
+
+
+
+
+
+function createDaysList(){
+  
+  const dezDaysList = [28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  const holidays = [24, 25, 31]
+  const fridays = [3, 10, 17, 24, 31]
+  numberDays = document.querySelector('#days')
+
+  
+  for (let i = 0; i < dezDaysList.length; i += 1){
+    let numberDaysItem = dezDaysList[i]
+    let newLi = document.createElement('li')
+    newLi.innerHTML = numberDaysItem
+    newLi.className = 'day'
+    for(n = 0; n < holidays.length; n += 1) {
+      if(parseInt(newLi.innerText) === holidays[n]) {
+        newLi.classList.add('holiday');
+      }
+    } 
+    for(n = 0; n < fridays.length; n += 1) {
+      if(parseInt(newLi.innerText) === fridays[n]) {
+        newLi.classList.add('friday');
+      }
+    } 
+    numberDays.appendChild(newLi)
+  }
+}
+
+createDaysList();
